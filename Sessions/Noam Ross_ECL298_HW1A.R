2eed4d578bd5c1b9aa2e96d64118eb8e222b74d2
noamross <noam.ross@gmail.com>
2011-02-02 09:35:43 -0800

    updated hastings model

---
+++ Sessions/Noam Ross_ECL298_HW1A.R
@@ -0,0 +1,38 @@
+rm(list=ls()) #clear workspace
+
+#set parameters
+lambda = 20 #population growth coefficient
+a = 1 #intraspecific competition term
+d = 0.04 #dispersal coefficient
+p = 2 #number of patches
+#N0 = runif(p)*10 #initial conditions
+tf = 500 #number of time steps
+N0s = seq(0,lambda/exp(1),length.out=100) #vector of initial conditions to test
+
+
+#create dispersal matrix
+D = matrix(d/(p-1),p,p) #create disperal matrix with uniform dispersal
+diag(D) = 1-d
+
+N = matrix(0,tf,p) #create variable space for individual run outputs
+final.Ns = array(0, dim = c(rep(length(N0s), p), p))
+
+ricker.growth = function(N, lambda, a) { #density dependent growth function
+	N2 = lambda*N*exp(-a*N)
+	return(N2)
+	}
+
+
+for(k in 1:length(N0s)) {
+  for(j in 1:length(N0s)) {
+    N[1,] = c(N0s[j], N0s[k]) #set initial conditions from N0s
+
+    for(i in 1:(tf-1)) { #for each time i...
+    	N[i+1,] = D%*%ricker.growth(N[i,], lambda, a) #calculate next values of N
+    	}
+    	
+    final.Ns[k,j,] = N[tf,]
+    }
+  }
+
+image(N0s,N0s,log(abs(final.Ns[,,1] - final.Ns[,,2])))
